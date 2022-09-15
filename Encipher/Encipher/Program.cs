cbaebb6c8531d482536b482fa25ec8eb4ca327fe
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2022-09-15 18:49:00 +0100

    added the caesar cipher to the main file
    - Added PlainText and CipherToUse arguments to the EncipherText function
    - Added a switch to determine what cipher to use
    - Added the caesar cipher to a case in the switch described above
    - Added a return for the resulting text
    
    modified: Encipher/Encipher/Program.cs
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

--- Encipher/Encipher/Program.cs
+++ Encipher/Encipher/Program.cs
@@ -8,9 +8,23 @@ namespace Encipher
 {
     public class Encipher
     {
-        public static string EncipherText()
+        public static string EncipherText(string PlainText, string CipherToUse)
         {
-            return "PLACEHOLDER";
+            string ResultingText;
+
+            switch (CipherToUse)
+            {
+                case "caesar cipher":
+                    Console.WriteLine("Please input your desired key for the caesar cipher. Only enter numbers.");
+                    string KeyInput = Console.ReadLine();
+                    int Key = int.Parse(KeyInput);
+                    ResultingText = CaesarCipher.CaesarCipherFunction(PlainText, Key);
+                    break;
+                default:
+                    throw new Exception("Cipher input not recognised. Please only enter supported cipher names from the documentation.");
+            }
+
+            return ResultingText
         }
     }
 }
