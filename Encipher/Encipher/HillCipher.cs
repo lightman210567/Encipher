bd59866c20abf3520903cb1a84cec643e9f15f85
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2022-11-12 12:30:42 +0000

    added a file for the hill cipher
    - Added a file to contain the encryption and decryption code for the hill cipher
    - Added the empty HillCipherFunction to contain future code
    
    new file: Encipher/Encipher/HillCipher.cs
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

---
+++ Encipher/Encipher/HillCipher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lightman210567.LetterToNumber;
+
+namespace Encipher
+{
+    internal class HillCipher
+    {
+        internal static string HillCipherFunction()
+        {
+
+        }
+    }
+}
