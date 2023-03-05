def27912cabe661c101382f67e19d5e765f277ca
= <81330314+lightman210567@users.noreply.github.com>
2023-03-05 19:17:58 +0000

    created the atbash ReverseLetter method
    The ReverseLetter method takes a character as an input and reverses it - this inverts the alphabet.
    For example a becomes z, b becomes y, etc.
    
    - Created the ReverseLetter method in the AtbashCipher class
    
    Signed-off-by: = <81330314+lightman210567@users.noreply.github.com>

--- Encipher/Encipher/AtbashCipher.cs
+++ Encipher/Encipher/AtbashCipher.cs
@@ -11,5 +11,68 @@ namespace Encipher
         internal static string Atbash(string PlainText) {
 
         }
+
+        internal static char ReverseLetter(char Letter)
+        {
+            Letter = Char.ToLower(Letter);
+
+            switch (Letter)
+            {
+                case 'a':
+                    return 'z';
+                case 'b':
+                    return 'y';
+                case 'c':
+                    return 'x';
+                case 'd':
+                    return 'w';
+                case 'e':
+                    return 'v';
+                case 'f':
+                    return 'u';
+                case 'g':
+                    return 't';
+                case 'h':
+                    return 's';
+                case 'i':
+                    return 'r';
+                case 'j':
+                    return 'q';
+                case 'k':
+                    return 'p';
+                case 'l':
+                    return 'o';
+                case 'm':
+                    return 'n';
+                case 'n':
+                    return 'm';
+                case 'o':
+                    return 'l';
+                case 'p':
+                    return 'k';
+                case 'q':
+                    return 'j';
+                case 'r':
+                    return 'i';
+                case 's':
+                    return 'h';
+                case 't':
+                    return 'g';
+                case 'u':
+                    return 'f';
+                case 'v':
+                    return 'e';
+                case 'w':
+                    return 'd';
+                case 'x':
+                    return 'c';
+                case 'y':
+                    return 'b';
+                case 'z':
+                    return 'a';
+                default:
+                    return ' ';
+            }
+        }
     }
 }
