bbb31734af8411e5de25d9421914c72a9536f3d1
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2023-03-06 16:02:28 +0000

    removed the EncipherText method
    The EncipherText method in the Encipher class has been removed to allow for a more user friendly system to be created
    
    - Removed the EncipherText method from the Encipher class
    
    modified: Encipher/Encipher/Program.cs
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

--- Encipher/Encipher/Program.cs
+++ Encipher/Encipher/Program.cs
@@ -8,26 +8,6 @@ namespace Encipher
 {
     public class Encipher
     {
-        public static string EncipherText(string PlainText, string CipherToUse)
-        {
-            string ResultingText = "";
-
-            switch (CipherToUse)
-            {
-                case "caesar cipher":
-                    Console.WriteLine("Please input your desired key for the caesar cipher. Only enter numbers.");
-                    string KeyInput = Console.ReadLine();
-                    int Key = int.Parse(KeyInput);
-                    ResultingText = CaesarCipher.CaesarCipherFunction(PlainText, Key);
-                    break;
-                case "atbash cipher":
-                    ResultingText = AtbashCipher.Atbash(PlainText);
-                    break;
-                default:
-                    throw new Exception("Cipher input not recognised. Please only enter supported cipher names from the documentation.");
-            }
-
-            return ResultingText;
-        }
+        
     }
 }
