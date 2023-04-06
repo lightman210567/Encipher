d237af952898c62e859a26f6372926bd71fd85a0
= <81330314+lightman210567@users.noreply.github.com>
2023-04-06 19:44:47 +0100

    added a method to contain the vigenere encryption code
    - Created a new class : VigenereCipher
    - Created a new method to contain the vigenere encryption code : VigenereEncrypt()
    
    new file:   Encipher/VigenereCipher.cs
    
    Signed-off-by: = <81330314+lightman210567@users.noreply.github.com>

---
+++ Encipher/Encipher/VigenereCipher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lightman210567.Encipher
+{
+    internal class VigenereCipher
+    {
+        internal static string VigenereEncrypt(string Key, string PlainText)
+        {
+            return "PLACEHOLDER";
+        }
+    }
+}
