299fb89ad3b27bccda6df7881f7ec03f7a96c7b1
= <81330314+lightman210567@users.noreply.github.com>
2023-04-06 20:34:59 +0100

    added vigenere key wrap around
    - Added code that "wraps around" the origional key if it is not as long as the plain text
    
    modified: Encipher/VigenereCipher.cs
    
    Signed-off-by: = <81330314+lightman210567@users.noreply.github.com>

--- Encipher/Encipher/VigenereCipher.cs
+++ Encipher/Encipher/VigenereCipher.cs
@@ -10,6 +10,57 @@ namespace lightman210567.Encipher
     {
         internal static string VigenereEncrypt(string Key, string PlainText)
         {
+            char[] PlainTextArray = PlainText.ToCharArray();
+            char[] KeyArray = Key.ToCharArray();
+            char[] ExtendedKeyArray = new char[PlainTextArray.Length];
+
+            bool KeyExtended = false;
+
+            if (KeyArray.Length == PlainTextArray.Length) // Check if the key is the same length as the plain text
+            {
+
+            }
+            else
+            {
+                KeyExtended = true;
+
+                int ArrayIndex = 0;
+                foreach (char letter in KeyArray) // Copy KeyArray contents into ExtendedKeyArray
+                {
+                    ExtendedKeyArray[ArrayIndex] = KeyArray[ArrayIndex];
+                    ArrayIndex++;
+                }
+
+                int Difference = PlainTextArray.Length - KeyArray.Length; // The number of key characters that need to be added
+
+                while (Difference > 0) // Until the length of the plaintext is equal to the length of the new key array
+                {
+                    int KeyLength = KeyArray.Length;
+                    int KeyArrayIndex = 0;
+
+                    foreach (char KeyLetter in KeyArray) // for every character in the key array :
+                    {
+                        if (Difference > 0) // if difference is not equal to 0
+                        {
+                            ExtendedKeyArray[ArrayIndex] = KeyArray[KeyArrayIndex]; // Set the value of ExtendedKeyArray[ArrayIndex] to KeyArray[KeyArrayIndex]
+
+                            ArrayIndex++;
+                            KeyArrayIndex++;
+                            Difference--;
+
+                            if (KeyArrayIndex > KeyLength) // reset the KeyArrayIndex if it is greater than the KeyLength
+                            {
+                                KeyArrayIndex = 0;
+                            }
+                        }
+                        else // if the difference is 0, break out of the loop
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
             return "PLACEHOLDER";
         }
     }
